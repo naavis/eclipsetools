d07a083054923f07ca6d6bd4b38341aba10933c1
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2025-06-15 10:23:09 +0300

    Add noise to translated test image
    This simulates varying noise in real images. The alignment procedure
    must be able to deal with some noise.

--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -8,14 +8,21 @@ from alignment import find_translation
 
 def test_single_translate():
     ref_image = utils.raw_reader.open_raw_image(r'images\eclipse_5ms.CR3')
-    offset_y = 42.1
+    offset_y = 42.2
     offset_x = -12.5
 
     translation_matrix = np.array([
         [1, 0, offset_x],
         [0, 1, offset_y]
     ], dtype=np.float32)
-    translated_image = cv2.warpAffine(ref_image, translation_matrix, dsize=(ref_image.shape[1], ref_image.shape[0]))
+    translated_image = cv2.warpAffine(
+        ref_image,
+        translation_matrix,
+        dsize=(ref_image.shape[1], ref_image.shape[0]))
+
+    # We add some Gaussian noise to the translated image to simulate varying noise in real images
+    rng = np.random.default_rng(122807528840384100672342137672332424406)
+    translated_image = np.clip(translated_image + rng.normal(0.0, 0.01, translated_image.shape), 0.0, 1.0)
 
     ref_image_preproc = preprocessing.preprocess_for_alignment(ref_image[50:-50, 50:-50, :])
     translated_image_preproc = preprocessing.preprocess_for_alignment(translated_image[50:-50, 50:-50, :])
@@ -23,4 +30,5 @@ def test_single_translate():
     found_translation = find_translation(ref_image_preproc, translated_image_preproc)
 
     total_error = np.sqrt(np.sum(np.square(found_translation - np.array([offset_y, offset_x]))))
+    print(f"Total error: {total_error}")
     assert total_error < 0.5
