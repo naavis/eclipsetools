22c1655d50e25ba513557d2bcdc9e413c7e2a7ea
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2025-06-15 16:37:26 +0300

    Use Click for command line arguments
    The Click library allows you to define command line arguments and such
    more easily.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import sys
+import click
 
-import eclipsetools.utils.raw_reader
-from eclipsetools import alignment
+from eclipsetools.alignment import find_translation
 from eclipsetools.preprocessing import preprocess_for_alignment
+from eclipsetools.utils.raw_reader import open_raw_image
 
 
-def main(args):
-    ref_image = preprocess_for_alignment(
-        eclipsetools.utils.raw_reader.open_raw_image(args[1]).data)
-    image_to_align = preprocess_for_alignment(
-        eclipsetools.utils.raw_reader.open_raw_image(args[2]).data)
-    print(alignment.find_translation(ref_image, image_to_align))
+@click.command()
+@click.argument('reference_image', type=click.Path(exists=True))
+@click.argument('image_to_align', type=click.Path(exists=True))
+def main(reference_image, image_to_align):
+    """
+    Align two eclipse images based on translation.
+
+    REFERENCE_IMAGE: Path to the reference RAW image file
+    IMAGE_TO_ALIGN: Path to the RAW image file that needs alignment
+    """
+    ref_image = preprocess_for_alignment(open_raw_image(reference_image).data)
+    img_to_align = preprocess_for_alignment(open_raw_image(image_to_align).data)
+    print(find_translation(ref_image, img_to_align))
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
