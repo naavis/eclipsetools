Fix broken rotation detection
Missing padding in the images was the original problem, and the pixel
shift was converted to degrees incorrectly. Now scaling and rotation
detection works, but the subsequent translation not.

eclipsetools/alignment.py
@@ -51,14 +51,17 @@ def find_transform(ref_image, image, low_pass_sigma):
              where dy, dx is the translation vector
     """
 
-    shape = ref_image.shape
+    ref_image_pad = _pad_with_zeros(ref_image)
+    image_pad = _pad_with_zeros(image)
+
+    shape = ref_image_pad.shape
     shortest_side = np.min(shape)
     radius = shortest_side // 8
 
     # Step 1: Find scale and rotation
     # We only need half of the log-polar FFTs, as they are symmetric
-    ref_fft_log_polar = _log_polar_fft(ref_image, radius)[:shape[0] // 2, :]
-    image_fft_log_polar = _log_polar_fft(image, radius)[:shape[0] // 2, :]
+    ref_fft_log_polar = _log_polar_fft(ref_image_pad, radius)[:shape[0] // 2, :]
+    image_fft_log_polar = _log_polar_fft(image_pad, radius)[:shape[0] // 2, :]
 
     # Find shifts in the log-polar FFTs using phase correlation
     shift_y, shift_x = _simple_phase_correlation(ref_fft_log_polar, image_fft_log_polar)
@@ -66,7 +69,7 @@ def find_transform(ref_image, image, low_pass_sigma):
     print(f'Found shifts: {shift_y=}, {shift_x=}')
 
     # Recover rotation from the correlation result
-    rotation_degrees = 360.0 * shift_y / (radius * np.pi * np.pi)
+    rotation_degrees = 360.0 * shift_y / ref_fft_log_polar.shape[0]
 
     # Recover scale from the correlation result
     klog = radius / np.log(radius)
@@ -74,19 +77,30 @@ def find_transform(ref_image, image, low_pass_sigma):
 
     # Step 2: Apply scale and rotation to the image
     derotate_rescale_matrix = cv2.getRotationMatrix2D(
-        (image.shape[1] // 2, image.shape[0] // 2), -rotation_degrees, 1.0 / scale)
+        (image_pad.shape[1] // 2, image_pad.shape[0] // 2), -rotation_degrees, 1.0 / scale)
 
     translated_image = cv2.warpAffine(
-        image,
+        image_pad,
         derotate_rescale_matrix,
-        dsize=(image.shape[1], image.shape[0]),
+        dsize=(image_pad.shape[1], image_pad.shape[0]),
         borderMode=cv2.BORDER_CONSTANT,
         borderValue=[0, 0, 0]).astype(np.float32)
 
     # Step 3: Find translation between reference and transformed image
-    translation = find_translation(ref_image, translated_image, low_pass_sigma)
+    translation_y, translation_x = find_translation(ref_image_pad, translated_image, low_pass_sigma)
 
-    return scale, rotation_degrees, translation
+    return float(scale), float(rotation_degrees), (float(translation_y), float(translation_x))
+
+
+def _pad_with_zeros(image: np.ndarray) -> np.ndarray:
+    assert len(image.shape) == 2, "Input image must be a 2D array (grayscale image)."
+    h, w = image.shape
+    longer_side = max(h, w)
+    padded = np.zeros((longer_side, longer_side), dtype=image.dtype)
+    y_offset = (longer_side - h) // 2
+    x_offset = (longer_side - w) // 2
+    padded[y_offset:y_offset + h, x_offset:x_offset + w] = image
+    return padded
 
 
 def _simple_phase_correlation(ref_image, image):