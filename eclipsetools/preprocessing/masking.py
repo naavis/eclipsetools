ad7520959be122db6f417ccd27a0e21d6f2bf1f2
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2025-06-28 23:41:22 +0300

    Blur annulus mask more
    Just in case the circles are still affecting the alignment.

--- eclipsetools/preprocessing/masking.py
+++ eclipsetools/preprocessing/masking.py
@@ -14,21 +14,25 @@ def annulus_mask(shape: np.ndarray,
                  mask_radius: float) -> np.ndarray:
     mask = np.zeros(shape, dtype=np.float32)
 
-    cv2.circle(img=mask,
-               center=(int(mask_center[1]), int(mask_center[0])),
-               radius=int(mask_radius * 2.0),
-               color=(1.0,),
-               thickness=-1)
-    cv2.circle(img=mask,
-               center=(int(mask_center[1]), int(mask_center[0])),
-               radius=int(mask_radius),
-               color=(0.0,),
-               thickness=-1)
-    mask = cv2.GaussianBlur(
+    cv2.circle(
+        img=mask,
+        center=(int(mask_center[1]), int(mask_center[0])),
+        radius=int(mask_radius * 2.0),
+        color=(1.0,),
+        thickness=-1)
+    cv2.circle(
+        img=mask,
+        center=(int(mask_center[1]), int(mask_center[0])),
+        radius=int(mask_radius),
+        color=(0.0,),
+        thickness=-1)
+    cv2.GaussianBlur(
         src=mask,
         ksize=(0, 0),
-        sigmaX=10,
-        sigmaY=10)
+        sigmaX=20,
+        sigmaY=20,
+        dst=mask)
+
     return mask
 
 
