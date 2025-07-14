678062b0106073803bd0834176496e4b564c9ef6
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2025-07-14 21:28:48 +0300

    Refactor masking code
    The structure is now a bit clearer, and also allows calculating the mask
    radii elsewhere when necessary.

--- eclipsetools/preprocessing/__init__.py
+++ eclipsetools/preprocessing/__init__.py
@@ -3,41 +3,37 @@ import numpy as np
 import eclipsetools.utils.circle_finder
 from eclipsetools.preprocessing import filtering
 from eclipsetools.preprocessing import masking
-from eclipsetools.preprocessing.masking import circle_mask
+from eclipsetools.preprocessing.masking import circle_mask, find_mask_radii_px
 
 
-def preprocess_for_alignment(rgb_image: np.ndarray, mask_inner_radius: float, mask_outer_radius: float) -> np.ndarray:
+def preprocess_for_alignment(rgb_image: np.ndarray,
+                             mask_inner_radius_multiplier: float,
+                             mask_outer_radius_multiplier: float) -> np.ndarray:
     image = np.mean(rgb_image, axis=2, dtype=np.float32)
     moon = eclipsetools.utils.circle_finder.find_circle(image, min_radius=400, max_radius=600)
     assert moon is not None, "Moon not found in the image. Please check the input image."
-
-    saturated_radius = masking.estimate_saturated_radius(moon, rgb_image)
-
-    mask_inner_radius_px = mask_inner_radius * (saturated_radius if saturated_radius else moon.radius)
-    mask_outer_radius_px = mask_outer_radius * mask_inner_radius_px
+    mask_inner_radius_px, mask_outer_radius_px = find_mask_radii_px(rgb_image,
+                                                                    moon,
+                                                                    mask_inner_radius_multiplier,
+                                                                    mask_outer_radius_multiplier)
 
     preproc = _mask_and_filter(image, moon.center, mask_inner_radius_px, mask_outer_radius_px)
     return preproc
 
 
-def _mask_and_filter(image: np.ndarray, moon_center: tuple, mask_inner_radius_px: float,
-                     mask_outer_radius_px: float) -> np.ndarray:
+def _mask_and_filter(image: np.ndarray,
+                     moon_center: tuple,
+                     mask_inner_radius_px: float,
+                     mask_outer_radius_px: float | None) -> np.ndarray:
     # TODO: Parametrize sigma, which is used to control the amount of rotational blur used in the tangential high-pass filter
     blurred_image = filtering.rotational_blur(image,
                                               sigma=2.0,
                                               center=moon_center)
     filtered_image = image - blurred_image
 
-    # We limit the outer radius of the annulus mask to avoid intersecting the image edges.
-    shortest_distance_to_edge = min(moon_center[0],
-                                    moon_center[1],
-                                    image.shape[0] - moon_center[0],
-                                    image.shape[1] - moon_center[1])
-    mask_outer_radius = min(mask_outer_radius_px, shortest_distance_to_edge * 0.95)
-
-    if mask_outer_radius_px > 0.0:
-        mask = masking.annulus_mask(image.shape, moon_center, mask_inner_radius_px, mask_outer_radius)
-    else:
+    if mask_outer_radius_px is None:
         mask = circle_mask(image.shape, moon_center, mask_inner_radius_px)
+    else:
+        mask = masking.annulus_mask(image.shape, moon_center, mask_inner_radius_px, mask_outer_radius_px)
     image_for_alignment = mask * filtered_image
     return image_for_alignment
--- eclipsetools/preprocessing/masking.py
+++ eclipsetools/preprocessing/masking.py
@@ -6,6 +6,12 @@ import numpy as np
 from eclipsetools.utils.circle_finder import DetectedCircle
 
 
+class MaskMode(StrEnum):
+    AUTO_PER_IMAGE = 'auto'
+    MAXIMUM = 'max'
+    FIXED_PIXELS = 'fixed'
+
+
 def hann_window_mask(shape: tuple) -> np.ndarray:
     assert len(shape) == 2, "Shape must be a 2D tuple (height, width)."
     return cv2.createHanningWindow(shape[::-1], cv2.CV_32F)
@@ -61,17 +67,30 @@ def circle_mask(shape: np.ndarray,
     return mask
 
 
-def estimate_saturated_radius(moon_params: DetectedCircle,
-                              raw_image: np.ndarray) -> float | None:
+def find_mask_radii_px(image: np.ndarray,
+                       moon: DetectedCircle,
+                       mask_inner_radius_multiplier: float,
+                       mask_outer_radius_multiplier: float) -> tuple[float, float | None]:
+    saturated_radius = _estimate_saturated_radius(moon, image)
+    mask_inner_radius_px = mask_inner_radius_multiplier * (saturated_radius if saturated_radius else moon.radius)
+
+    shortest_distance_to_edge = min(moon.center[0],
+                                    moon.center[1],
+                                    image.shape[0] - moon.center[0],
+                                    image.shape[1] - moon.center[1])
+
+    # We limit the outer radius of the annulus mask to avoid intersecting the image edges.
+    mask_outer_radius_px = min(mask_outer_radius_multiplier * mask_inner_radius_px,
+                               shortest_distance_to_edge * 0.95) if mask_outer_radius_multiplier > 0.0 else None
+
+    return mask_inner_radius_px, mask_outer_radius_px
+
+
+def _estimate_saturated_radius(moon_params: DetectedCircle,
+                               raw_image: np.ndarray) -> float | None:
     saturated_pixels = cv2.erode(np.max(raw_image, axis=2), kernel=np.ones((3, 3), np.float32), iterations=1) > 0.999
     saturated_radius = None
     if np.any(saturated_pixels):
         distances = np.linalg.norm(np.argwhere(saturated_pixels) - moon_params.center, axis=1)
         saturated_radius = np.max(distances)
     return saturated_radius
-
-
-class MaskMode(StrEnum):
-    AUTO_PER_IMAGE = 'auto'
-    MAXIMUM = 'max'
-    FIXED_PIXELS = 'fixed'
